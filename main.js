93b1f1d0858173afe42148c174cc0cb7b3be8c47
PenguinCabinet <kage2.kage22@gmail.com>
2024-03-05 12:27:41 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,68 @@
+﻿function make_output(input_text,times_text){
+    const template_str="19:00〜";
+
+    const times_arr=
+        times_text
+        .split("\n")
+        .filter((e)=>e!="");
+    
+    if(times_arr.length==0)
+        return input_text;
+
+    const input_arr=
+        input_text
+        .split("\n")
+        .filter((e)=>e!="");;
+
+    let out_arr=
+        input_arr.map(text=>{
+            let ret=
+                times_arr
+                .map(e => {
+                    return text.replace(template_str,e);
+                });
+            
+            return ret.join("\n");
+        })
+        
+
+    return out_arr.join("\n");
+}
+
+function make_output_wrapper(){
+    const out_text=make_output(
+        document.getElementById("input_text").value,
+        document.getElementById("times_text").value
+    );
+    document.getElementById("output_text").value=out_text;
+}
+
+
+["input_text","times_text"]
+.forEach(e => {
+    document.getElementById(e).addEventListener(
+        'keydown', 
+        make_output_wrapper
+    );
+    document.getElementById(e).addEventListener(
+        'change', 
+        make_output_wrapper
+    );
+})
+
+function output_text_copy() {
+    navigator.clipboard.writeText(document.getElementById("output_text").value).then(
+        () => {
+        },
+        () => {
+        },
+    );
+}
+
+document.getElementById("output_text").placeholder=
+    make_output(
+        document.getElementById("input_text").placeholder,
+        document.getElementById("times_text").placeholder
+    );
+
+//make_output_wrapper();
